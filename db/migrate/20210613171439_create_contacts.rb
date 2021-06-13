e788db532b51d9f0ab4d9074c1341d603557ad3b
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-13 17:20:34 +0000

    [Add] Add columns to 20210613171439_create_contacts

--- db/migrate/20210613171439_create_contacts.rb
+++ db/migrate/20210613171439_create_contacts.rb
@@ -1,7 +1,13 @@
 class CreateContacts < ActiveRecord::Migration[5.2]
   def change
     create_table :contacts do |t|
-
+      ## Column
+      t.integer :customer_id, null: false
+      t.string :name, null: false
+      t.string :phone_number, null: false
+      t.string :email
+      t.string :subject, null: false
+      t.string :message, null: false
       t.timestamps
     end
   end
