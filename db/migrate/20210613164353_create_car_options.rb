5d2754d02a5efc7d3f98089a3a3487b142a75904
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-13 16:46:30 +0000

    [Add] Add columns to 20210613164353_create_car_options

--- db/migrate/20210613164353_create_car_options.rb
+++ db/migrate/20210613164353_create_car_options.rb
@@ -1,7 +1,11 @@
 class CreateCarOptions < ActiveRecord::Migration[5.2]
   def change
     create_table :car_options do |t|
-
+      ## Column
+      t.integer :car_id, null: false
+      t.string :name, null: false
+      t.integer :price, null: false
+      t.boolean :is_active, default: true, null: false
       t.timestamps
     end
   end
