417ccc94dab4c0274662c898a050f75d96af87ad
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-13 16:58:02 +0000

    [Add] Add columns to 20210613165126_create_blogs

--- db/migrate/20210613165126_create_blogs.rb
+++ db/migrate/20210613165126_create_blogs.rb
@@ -1,7 +1,11 @@
 class CreateBlogs < ActiveRecord::Migration[5.2]
   def change
     create_table :blogs do |t|
-
+      ## Column
+      t.integer :customer_id, null: false
+      t.string :title, null: false
+      t.text :body, null: false
+      t.string :image_id
       t.timestamps
     end
   end
