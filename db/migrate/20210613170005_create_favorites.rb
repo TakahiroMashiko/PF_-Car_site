0d903b2494f42a67aad69e0adb935c471fee002c
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-13 17:02:12 +0000

    [Add] Add column to 20210613170005_create_favorites

--- db/migrate/20210613170005_create_favorites.rb
+++ db/migrate/20210613170005_create_favorites.rb
@@ -1,7 +1,7 @@
 class CreateFavorites < ActiveRecord::Migration[5.2]
   def change
     create_table :favorites do |t|
-
+      t.integer :customer_id, null: false
       t.timestamps
     end
   end
