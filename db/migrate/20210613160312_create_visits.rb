b5a544e7184c39161b023b45b5b4d51c160c34ae
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-13 16:16:29 +0000

    [Add] Add columns to 20210613160312_create_visits

--- db/migrate/20210613160312_create_visits.rb
+++ db/migrate/20210613160312_create_visits.rb
@@ -1,7 +1,15 @@
 class CreateVisits < ActiveRecord::Migration[5.2]
   def change
     create_table :visits do |t|
-
+      ## Column
+      t.integer :customer_id, null: false
+      t.integer :dealer_id, null: false
+      t.string :name, null: false
+      t.string :name_kana, null: false
+      t.string :phone_number, null: false
+      t.string :email, null: false
+      t.string :visit, null: false
+      t.integer :visit_status, default: 0, null: false
       t.timestamps
     end
   end
