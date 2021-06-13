4850a9a4929a3710b996279001c0a235593a9698
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-13 15:41:18 +0000

    [Add] Add columns to 20210613153606_devise_create_customers

--- db/migrate/20210613153606_devise_create_customers.rb
+++ db/migrate/20210613153606_devise_create_customers.rb
@@ -32,6 +32,15 @@ class DeviseCreateCustomers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      ## Column
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.string :last_name_kana, null: false
+      t.string :first_name_kana, null: false
+      t.string :email, null: false
+      t.string :encrypted_password, null: false
+      t.string :phone_number, null: false
+      t.boolean :is_active, default: true, null: false
 
       t.timestamps null: false
     end
