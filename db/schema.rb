2e004171f8ce3a440c03d1dc8917f0b71d26a645
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-13 16:18:12 +0000

    [Update] DB migrate visits to db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_13_154911) do
+ActiveRecord::Schema.define(version: 2021_06_13_160312) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -40,6 +40,19 @@ ActiveRecord::Schema.define(version: 2021_06_13_154911) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "visits", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "dealer_id", null: false
+    t.string "name", null: false
+    t.string "name_kana", null: false
+    t.string "phone_number", null: false
+    t.string "email", null: false
+    t.string "visit", null: false
+    t.integer "visit_status", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
