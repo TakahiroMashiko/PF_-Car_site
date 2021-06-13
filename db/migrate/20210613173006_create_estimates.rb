196b21421870bc33369ccebcb07d6b889c36dde7
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-13 17:35:51 +0000

    [Add] Add columns to 20210613173006_create_estimates

--- db/migrate/20210613173006_create_estimates.rb
+++ db/migrate/20210613173006_create_estimates.rb
@@ -1,7 +1,14 @@
 class CreateEstimates < ActiveRecord::Migration[5.2]
   def change
     create_table :estimates do |t|
-
+      ## Column
+      t.integer :customer_id, null: false
+      t.integer :car_id, null: false
+      t.integer :car_option_id, null: false
+      t.integer :total, null: false
+      t.integer :tax, null: false
+      t.integer :car_price, null: false
+      t.integer :option_price, null: false
       t.timestamps
     end
   end
