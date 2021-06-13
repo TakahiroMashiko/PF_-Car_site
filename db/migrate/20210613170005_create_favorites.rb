fe0b6c469a1e859dde262cb7e7222baf4840c039
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-13 17:09:21 +0000

    [Add] Add columns to 20210613170459_create_dealers

--- db/migrate/20210613170005_create_favorites.rb
+++ db/migrate/20210613170005_create_favorites.rb
@@ -1,6 +1,7 @@
 class CreateFavorites < ActiveRecord::Migration[5.2]
   def change
     create_table :favorites do |t|
+      ## Column
       t.integer :customer_id, null: false
       t.timestamps
     end
--- db/migrate/20210613170459_create_dealers.rb
+++ db/migrate/20210613170459_create_dealers.rb
@@ -1,7 +1,10 @@
 class CreateDealers < ActiveRecord::Migration[5.2]
   def change
     create_table :dealers do |t|
-
+      ## Column
+      t.string :address, null: false
+      t.string :phone_number, null: false
+      t.string :image_id
       t.timestamps
     end
   end
