fa12c30fb58643ab34906453d9cceae61e5a2f87
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-13 16:46:57 +0000

    [Update] DB migrate car_options to db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_13_164120) do
+ActiveRecord::Schema.define(version: 2021_06_13_164353) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2021_06_13_164120) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "car_options", force: :cascade do |t|
+    t.integer "car_id", null: false
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "cars", force: :cascade do |t|
