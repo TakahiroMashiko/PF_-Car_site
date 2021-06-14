676216b3539fe60f1f9be6a9d1772e08842d69cf
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-14 15:06:25 +0000

    [Update] Set devise strong parameters to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  # New registration save function
 end
