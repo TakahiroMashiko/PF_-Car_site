c3b8c8e853792aa9cac9f0f2a10f4cd249b278f6
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-16 16:04:22 +0000

    [Update] Add devise_scope :customer do to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,12 @@ Rails.application.routes.draw do
   end
 
   # customer
-  devise_for :customers
+  devise_for :customers, skip: :all
+  devise_scope :customer do
+      get 'customers/sign_up' => 'customers/registrations#new', as: :new_customer_registration
+      post 'customers/sign_up' => 'customers/registrations#create', as: :customer_registration
+      get 'customers/sign_in' => 'customers/sessions#new', as: :new_customer_session
+      post 'customers/sign_in' => 'customers/sessions#create', as: :customer_session
+      delete '/customers/sign_out' => 'customers/sessions#destroy', as: :destroy_customer_session
+  end
 end
