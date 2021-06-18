0882facc73ebb917b6352c900cad2ff606e53d22
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-18 16:47:35 +0000

    [Update] Add validates :genre_id, :name, :price, presence: true to car.rb

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,5 +2,8 @@ class Car < ApplicationRecord
   belongs_to :genre
   has_many :car_options, dependent: :destroy
   has_many :estimates, dependent: :destroy
+
   attachment :image
+
+  validates :genre_id, :name, :price, presence: true
 end
