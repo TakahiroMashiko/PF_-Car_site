2a7431428705b4547ec6ffefdff4e82d832ab9c6
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-19 17:11:49 +0000

    [Update] Adddef index,new,create,show,edit,update to admin/car_options_controller.rb

--- app/controllers/admin/car_options_controller.rb
+++ app/controllers/admin/car_options_controller.rb
@@ -1,19 +1,47 @@
 class Admin::CarOptionsController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
+    @car_options = Car_option.all.page(params[:page]).per(10)
   end
 
   def new
+    @car_option = Car_option.new
   end
 
   def create
+    @car_option = Car_option.new(car_params)
+    if @car_option.save
+      flash[:notice] = "新オプションを登録しました"
+      redirect_to admin_car_option_path(@car_option.id)
+    else
+      render "new"
+    end
   end
 
   def show
+    @car_option = Car_option.find(params[:id])
+    # Tax
+    @tax = 1.10
   end
 
   def edit
+    @car_option = Car_option.find(params[:id])
   end
 
   def update
+    @car_option = Car_option.find(params[:id])
+    if @car_option.update(car_params)
+      flash[:notice] = "オプションの情報を更新しました"
+      redirect_to admin_car_option_path(@car_option.id)
+    else
+      render "edit"
+    end
+  end
+
+  # Strong parameters
+  private
+  def car_option_params
+    params.require(:car_option).permit(:car_id, :name, :price, :is_active)
   end
 end
