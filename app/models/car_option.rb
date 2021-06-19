e5ebaccd20cbf9ce7c696b2f83f4f200babb31da
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-19 17:27:11 +0000

    [Update] Addt belongs_to :car,estimate,validates :car_id, :name, :price, presence: true,validates :price, numericality: { only_integer: true } to car_option.rb

--- app/models/car_option.rb
+++ app/models/car_option.rb
@@ -1,2 +1,7 @@
 class CarOption < ApplicationRecord
+  belongs_to :car
+  belongs_to :estimate
+
+  validates :car_id, :name, :price, presence: true
+  validates :price, numericality: { only_integer: true }
 end
