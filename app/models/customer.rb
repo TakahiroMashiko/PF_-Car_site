920f2e4d71fa5ebc727f655516ccd9190eefcc48
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-20 04:37:49 +0000

    [Update] Add validates :last_name_kana, :first_name_kana,format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ } to customer.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,4 +6,6 @@ class Customer < ApplicationRecord
 
   validates :last_name, :first_name, :last_name_kana, :first_name_kana, :phone_number, presence: true
   validates :phone_number, numericality: { only_integer: true }
+  validates :last_name_kana, :first_name_kana,
+    format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
 end
