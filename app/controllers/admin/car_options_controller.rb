fc29eeac71486b3580d3d6b19e8b3bec03cc6a04
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-20 03:57:10 +0000

    [Fix] Fix @caroption to admin/car_options_controller.rb

--- app/controllers/admin/car_options_controller.rb
+++ app/controllers/admin/car_options_controller.rb
@@ -2,38 +2,38 @@ class Admin::CarOptionsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @car_options = CarOption.all.page(params[:page]).per(10)
+    @caroptions = CarOption.all.page(params[:page]).per(10)
   end
 
   def new
-    @car_option = CarOption.new
+  @caroption = CarOption.new
   end
 
   def create
-    @car_option = CarOption.new(car_params)
-    if @car_option.save
+    @caroption = CarOption.new(caroption_params)
+    if @caroption.save
       flash[:notice] = "新オプションを登録しました"
-      redirect_to admin_car_option_path(@car_option.id)
+      redirect_to admin_car_option_path(@caroption.id)
     else
       render "new"
     end
   end
 
   def show
-    @car_option = CarOption.find(params[:id])
+    @caroption = CarOption.find(params[:id])
     # Tax
     @tax = 1.10
   end
 
   def edit
-    @car_option = CarOption.find(params[:id])
+    @caroption = CarOption.find(params[:id])
   end
 
   def update
-    @car_option = CarOption.find(params[:id])
-    if @car_option.update(car_params)
+    @caroption = CarOption.find(params[:id])
+    if @caroption.update(car_params)
       flash[:notice] = "オプションの情報を更新しました"
-      redirect_to admin_car_option_path(@car_option.id)
+      redirect_to admin_car_option_path(@caroption.id)
     else
       render "edit"
     end
@@ -41,7 +41,7 @@ class Admin::CarOptionsController < ApplicationController
 
   # Strong parameters
   private
-  def car_option_params
+  def caroption_params
     params.require(:car_option).permit(:car_id, :name, :price, :is_active)
   end
 end
