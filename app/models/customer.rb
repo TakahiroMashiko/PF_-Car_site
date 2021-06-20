db8d30c4f181a44c0f5a8c8b629bb72c04fe438f
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-20 04:46:17 +0000

    [Update] Add has_many :visits to customer.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,8 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :visits
+
   validates :last_name, :first_name, :last_name_kana, :first_name_kana, :phone_number, presence: true
   validates :phone_number, numericality: { only_integer: true }
   validates :last_name_kana, :first_name_kana,
