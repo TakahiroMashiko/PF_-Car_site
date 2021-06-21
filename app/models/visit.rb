d8bac82b451a3fc94b5709c5262d366818b5b6be
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-21 11:16:10 +0000

    [Update] Add validates :name, :name_kana, :phone_number, :email, :visit, presence: true to visit.rb

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -3,4 +3,6 @@ class Visit < ApplicationRecord
   belongs_to :dealer
 
   enum visit_status: { 来店待ち: 0, 接客中: 1, 接客済み: 2, キャンセル: 3 }
+
+  validates :name, :name_kana, :phone_number, :email, :visit, presence: true
 end
