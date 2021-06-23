3e8c840d2ac793639d8cb563fbad37d7c666bfab
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-23 15:20:41 +0000

    [Update] Add def index @contacts = Contact.all.page(params[:page]).per(10) to admin/contacts_controller.rb

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -2,6 +2,7 @@ class Admin::ContactsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
+    @contacts = Contact.all.page(params[:page]).per(10)
   end
 
   def show
