cd6f4c6b1fc0460f6a4d1a767f4b1e9a83680cb1
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-23 11:35:16 +0000

    [Update] Add strong parameters  :image to admin/blogs_controller.rb

--- app/controllers/admin/blogs_controller.rb
+++ app/controllers/admin/blogs_controller.rb
@@ -50,6 +50,6 @@ class Admin::BlogsController < ApplicationController
   # Strong parameters
   private
   def blog_params
-    params.require(:blog).permit(:favorite_id, :image_id, :title, :body)
+    params.require(:blog).permit(:favorite_id, :image_id, :title, :body, :image)
   end
 end
