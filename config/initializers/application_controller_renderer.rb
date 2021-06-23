7072a071e52d929c8f14bdb25b6ee94d2731e6fd
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-23 11:31:33 +0000

    [Update] Add Refile.secret_key to application_controller_renderer.rb

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+# Refile.secret_key
+Refile.secret_key = '1ac75377fce03643b8ad7f53aeefee041e98cf17437ce58346a1dde1f4021b397e08b2204749cc03521cff67bd623ae5982c53f1b3c97131ee7a39f887220bbb'
