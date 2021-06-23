f75656f00bfa37a44411bd4f09532be40274b57e
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-23 15:23:06 +0000

    [Update] Add strong parameters to admin/contacts_controller.rb

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -8,4 +8,10 @@ class Admin::ContactsController < ApplicationController
   def show
     @contact = Contact.find(params[:id])
   end
+
+  # Strong parameters
+  private
+  def contact_params
+    params.require(:contact).permit(:customer_id, :name, :phone_number, :email, :subject, :message)
+  end
 end
