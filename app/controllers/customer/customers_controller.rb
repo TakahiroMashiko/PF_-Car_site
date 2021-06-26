c865ee098549020db9614a716dd94b678c1e9b40
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-26 23:00:08 +0000

    [Update] Add @customer = current_customer to customer/customers_controller.rb

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -2,6 +2,7 @@ class Customer::CustomersController < ApplicationController
   before_action :authenticate_customer!, only: [:show, :edit, :update, :withdrawal, :quit]
 
   def show
+    @customer = current_customer
   end
 
   def edit
