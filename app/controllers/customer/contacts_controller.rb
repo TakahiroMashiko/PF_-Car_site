60e5f3d44e962d6b928e7d16419659a9a584424f
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-27 13:53:18 +0000

    [Update] Add strong parameters to customer/contacts_controller.rb

--- app/controllers/customer/contacts_controller.rb
+++ app/controllers/customer/contacts_controller.rb
@@ -24,4 +24,10 @@ class Customer::ContactsController < ApplicationController
       render "new"
     end
   end
+
+  # Strong parameters
+  private
+  def contact_params
+    params.require(:contact).permit(:customer_id, :name, :phone_number, :email, :subject, :message)
+  end
 end
