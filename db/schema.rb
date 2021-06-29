59521d49831a0b2578f536f218585a5a39ad9e0a
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-29 10:14:59 +0000

    [Update] Add create_table favorites, force: :cascade do |t| t.integer blog_id to db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_23_144144) do
+ActiveRecord::Schema.define(version: 2021_06_29_101128) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(version: 2021_06_23_144144) do
     t.integer "customer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "blog_id"
   end
 
   create_table "genres", force: :cascade do |t|
