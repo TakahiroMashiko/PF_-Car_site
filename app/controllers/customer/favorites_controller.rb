a3f831f017d8e2adb9d03c76c42ad80cd816cea4
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-29 12:20:11 +0000

    [Update & Fix] Update def create blog = Blog.find(params[:id]),Fix def destroy to customer/favorites_controller.rb

--- app/controllers/customer/favorites_controller.rb
+++ app/controllers/customer/favorites_controller.rb
@@ -1,9 +1,13 @@
 class Customer::FavoritesController < ApplicationController
-  before_action :authenticate_customer!, only: [:create, :destoroy]
+  before_action :authenticate_customer!, only: [:create, :destroy]
 
   def create
+    blog = Blog.find(params[:id])
+    favorite = current_customer.favorites.new(blog_id: blog.id)
+    favorite.save
+    redirect_to blog_path(blog)
   end
 
-  def destoroy
+  def destroy
   end
 end
