1dd3377984f3be0532e5c92052d2b400ec940591
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-06-29 17:28:22 +0000

    [Update] Add def create redirect_to blog_path(@blog.id) to customer/favorites_controller.rb

--- app/controllers/customer/favorites_controller.rb
+++ app/controllers/customer/favorites_controller.rb
@@ -2,10 +2,10 @@ class Customer::FavoritesController < ApplicationController
   before_action :authenticate_customer!, only: [:create, :destroy]
 
   def create
-    blog = Blog.find(params[:id])
-    favorite = current_customer.favorites.new(blog_id: blog.id)
+    @blog = Blog.find(params[:blog_id])
+    favorite = current_customer.favorites.new(blog_id: params[:blog_id])
     favorite.save
-    redirect_to blog_path(blog)
+    redirect_to blog_path(@blog.id)
   end
 
   def destroy
