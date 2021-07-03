5a96324867258b886ef2707ee73d4410f45a5d97
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-07-03 15:12:37 +0000

    [Remove] Remove redirect_to blog_path(@blog.id) to customer/favorites_controller.rb

--- app/controllers/customer/favorites_controller.rb
+++ app/controllers/customer/favorites_controller.rb
@@ -5,13 +5,11 @@ class Customer::FavoritesController < ApplicationController
     @blog = Blog.find(params[:blog_id])
     favorite = current_customer.favorites.new(blog_id: params[:blog_id])
     favorite.save
-    redirect_to blog_path(@blog.id)
   end
 
   def destroy
     @blog = Blog.find(params[:blog_id])
     favorite = Favorite.find_by(blog_id: params[:blog_id], customer_id: current_customer.id)
     favorite.destroy
-    redirect_to blog_path(@blog.id)
   end
 end
