fba6e1ee87c1ae2b8c65cb47d53f96bfd5269ccc
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-07-09 13:02:30 +0000

    [Add] Add notification_mailer

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,13 @@
+class NotificationMailer < ApplicationMailer
+  default from: "sengoku.cars.smtp@gmail.com"
+
+  def send_confirm_to_customer(customer)
+    @customer = customer
+     mail(
+      subject: "[Sengoku Cars]モデルのお見積り",
+      to: @customer.email
+    ) do |format|
+      format.text
+    end
+  end
+end
