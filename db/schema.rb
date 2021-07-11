ead5548f8ff7b40ba9be98cc8e0fe8c2621ee154
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-07-11 01:57:34 +0000

    [Update] Add column :cars, :evaluation, :float

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_29_101128) do
+ActiveRecord::Schema.define(version: 2021_07_11_015559) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_06_29_101128) do
     t.boolean "is_active", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "evaluation"
   end
 
   create_table "contacts", force: :cascade do |t|
