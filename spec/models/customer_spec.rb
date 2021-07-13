f61f9aa2301af74e7af77798193752704ad58712
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-07-13 15:03:50 +0000

    [Update] Add do expect(FactoryBot.build(:customer, email: )).to_not be_valid to spec/models/customer_spec.rb

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -10,7 +10,9 @@ RSpec.describe Customer, type: :model do
     expect(FactoryBot.build(:customer, name: "")).to_not be_valid
   end
 
-  it "メールアドレスがなければ登録できない"
+  it "メールアドレスがなければ登録できない" do
+    expect(FactoryBot.build(:customer, email: "")).to_not be_valid
+  end
 
   it "メールアドレスが重複していたら登録できない"
 
