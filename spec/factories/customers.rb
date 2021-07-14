b08c608d5c4f1f4da2e9d28ae1204341f368fcb8
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2021-07-14 13:58:51 +0000

    [Update] Add factory :customer do sequence(:email),etc to spec/factories/customers.rb

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,7 +1,11 @@
 FactoryBot.define do
   factory :customer do
-    name {"taka"}
-    sequence(:email) { |n| "taka#{n}@example.com"}
-    password {"password"}
+    sequence(:email) { |n| "TEST#{n}@example.com"}
+    sequence(:last_name) { |n| "TEST_LAST_NAME#{n}"}
+    sequence(:first_name) { |n| "TEST_FIRST_NAME#{n}"}
+    sequence(:last_name_kana) { |n| "メイ#{n}"}
+    sequence(:first_name_kana) { |n| "セイ#{n}"}
+    sequence(:phone_number) { |n| "00000000000#{n}"}
+    sequence(:is_active) { |n| "1#{n}"}
   end
 end
